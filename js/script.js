726904d9c530f1bcba868b0f9c030aabde766390
dianasou <dsouthiphomma@gmail.com>
2021-10-25 19:33:31 -0500

    Update link decorations and add scroll down for contact

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ function menuToggle () {
   }
 }
 
-/* Open when someone clicks on the span element */
+/* open menu when someone clicks on menu span element */
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
+/* close menu when someone clicks on the "x" symbol */
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
