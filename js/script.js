dfa8930d7dd076560bed53b7f943a33581e91110
dianasou <dsouthiphomma@gmail.com>
2021-10-28 14:41:42 -0500

    Add contact page, form, and nav link

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-function menuToggle () {
-  var x = document.getElementById('myNav');
-  if (x.className === 'navbar') {
-    // add responsive to className to show menu
-    x.className += ' responsive';
-  } else {
-    // change className back to hide menu
-    x.className = 'navbar';
-  }
-}
-
 /* open menu when someone clicks on menu span element */
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
